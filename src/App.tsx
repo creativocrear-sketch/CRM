9624656d09dbe2e7599bde06e3e57fb1c1d1720e
creativocrear-sketch <creativocrear@gmail.com>
2026-03-24 17:51:06 -0500

    Create src/App.tsx file from the existing App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// This is a sample content for App.tsx
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+};
+
+export default App;
